0a9c75832bb0556906873d8977f64d918d9aaf91
solo3lihamed <fps60y@gmail.com>
2025-06-12 16:10:23 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}
+);
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+}
+);
+// Export the app for testing purposes
